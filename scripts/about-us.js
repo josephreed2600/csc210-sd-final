109d238901b82cfeabc10ccc07bccee92d5fa96f
Joseph Reed <josephreed2600@gmail.com>
2020-03-11 23:40:37 -0600

    about us script

---
+++ scripts/about-us.js
@@ -0,0 +1,22 @@
+window.onload = function(){
+var canvas = document.getElementById('barGraph');
+var ctx = canvas.getContext('2d');
+    ctx.fillStyle = "#C0C5C1";
+    ctx.fillRect(25, 475, 50, 25);
+    ctx.fillStyle = "#C0C5C1";
+    ctx.fillRect(125, 462.5, 50, 37.5);
+    ctx.fillStyle = "#C0C5C1";
+    ctx.fillRect(225, 450, 50, 50);
+    ctx.fillStyle = "#C0C5C1";
+    ctx.fillRect(325, 412.5, 50, 87.5);
+    ctx.fillStyle = "#C0C5C1";
+    ctx.fillRect(425, 375, 50, 125);
+    ctx.fillStyle = "#C0C5C1";
+    ctx.fillRect(525, 297.5, 50, 202.5);
+    ctx.fillStyle = "#C0C5C1";
+    ctx.fillRect(625, 200, 50, 300);
+    ctx.fillStyle = "#C0C5C1";
+    ctx.fillRect(725, 100, 50, 400);
+    ctx.fillStyle = "#C0C5C1";
+    ctx.fillRect(825, 0, 50, 500);
+}
